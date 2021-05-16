a06c828f2b4216afc40b974ed8d64eedca72a2f2
Remco van de Siepkamp <rvandesiepkamp@gmail.com>
2021-05-16 13:33:41 +0200

    Level_18_Arrow-functions

---
+++ script.js
@@ -0,0 +1,35 @@
+// deel A
+
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+  };
+  
+  ikRockArrowFunctions()
+
+
+  // deel B
+
+  const fivePlusSeven = () => 5 + 7
+  
+  fivePlusSeven();  
+
+
+  // deel C
+
+  const myFunction = (a, b) => a + b
+  
+  myFunction();
+
+
+  // deel D
+
+  const addFive = a => a + 5
+
+  addFive();
+
+
+  // deel E
+
+  const createObject = () => ({greeting: "hoi"})
+
+  createObject();
